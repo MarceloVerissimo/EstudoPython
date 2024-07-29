55e352c1b5a1c8b106bc1c7fa52d823e04df44fc
MarceloVerissimo <verissimomarcelo95@gmail.com>
2024-07-29 20:17:42 -0300

    aulaspyhton

---
+++ aula1.py
@@ -0,0 +1,19 @@
+"""
+DocString (Que não é um comentários mas pode ser usado pra isso)- Multiplas linhas de comentários
+E escrever o que eu
+quiseder
+
+
+qualquer coisa
+"""
+
+'''
+Usar para escrever notas
+'''
+
+
+
+# Permite escrever um comentário
+print(123) #Na frente
+# Abaixo
+print(123)
---
+++ aula3.py
@@ -0,0 +1,14 @@
+# Strings são textos que vem geralmente entre aspas
+
+# Aspas Simples
+print('Marcelo Verissimo')
+
+# Aspas duplas
+print("Marcelo Verissimo")
+
+# Escape
+print("Marcelo \"Verissimo")
+
+# r
+print(r"Marcelo \"Verissimo")
+print('Explícito', 'é', 'melhor " do que implícito')
